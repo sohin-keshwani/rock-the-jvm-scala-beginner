92945eec2eda5c2f7153cce51a5deb80bcef1ef6
Sohin Keshwani <sohin.keshwani@viacom18.com>
2022-05-09 14:01:56 +0530

    reaction factory for kids

--- src/main/scala/lectures/part2oop/Enums.scala
+++ src/main/scala/lectures/part2oop/Enums.scala
@@ -21,4 +21,66 @@ object Enums extends App {
   println(Permissions.valueOf("READ"))
 
 
+  enum ReactionEventType(reaction: String) {
+    case FAVOURITE(var value: String, var t: String) extends ReactionEventType("favourite")
+    case UNFAVOURITE(var value: String, var t: String) extends ReactionEventType("unfavourite")
+
+    var variableOne: String = ""
+    var variableTwo: String = ""
+
+    def handleReaction() : Unit = {
+      if (this == FAVOURITE) {
+        variableOne = "1"
+        variableTwo = "2"
+      } else if (this == UNFAVOURITE) {
+        variableOne = "3"
+        variableTwo = "4"
+
+      } else {
+        throw new RuntimeException
+      }
+    }
+
+  }
+
+  object ReactionEventType {
+    def getReactionFromType(reaction: String) : ReactionEventType = {
+      reaction match  {
+        case "favourite" => ReactionEventType.FAVOURITE("1", "2")
+        case "unfavourite" => ReactionEventType.UNFAVOURITE("1", "2")
+        case _ => throw new RuntimeException("Reaction type not supported")
+      }
+    }
+  }
+
+  val reactionType: ReactionEventType = ReactionEventType.getReactionFromType("favourite")
+
+
+  trait ReactionEventTypeTrait {
+    def performSomeActionBasedOnReaction: Unit
+  }
+
+  class FavouriteEventType extends ReactionEventTypeTrait {
+    override def performSomeActionBasedOnReaction: Unit = {
+      println("I am favourite")
+    }
+  }
+
+  class UnFavouriteEventType extends ReactionEventTypeTrait {
+    override def performSomeActionBasedOnReaction: Unit = {
+      println("I am UnFavourite")
+    }
+  }
+
+  object ReactionEventTypeFactoy {
+    def apply(eventCode : Int) : ReactionEventTypeTrait = {
+      eventCode match {
+        case 6 => new FavouriteEventType
+        case 7 => new UnFavouriteEventType
+        case _ => throw new RuntimeException
+      }
+    }
+  }
+
+  ReactionEventTypeFactoy(6).performSomeActionBasedOnReaction
 }
