babca2c74f324b9c97b876de0776bd21f2ccf200
Sohin Keshwani <sohin.keshwani@viacom18.com>
2022-05-18 14:54:01 +0530

    tupples and wip social network basics

--- src/main/scala/lectures/part3fp/TuplesAndMaps.scala
+++ src/main/scala/lectures/part3fp/TuplesAndMaps.scala
@@ -23,7 +23,7 @@ object TuplesAndMaps extends App {
   println(phonebook.toList)
   println(List(("shohin", 123)).toMap)
 
-  val names = List("fai", "iffi", "mosh", "shohin", "sufii")
+  val names = List("fai", "iffi", "mosh", "shohin", "sufi")
 
   println(names.groupBy(name => name.charAt(0)))
 
